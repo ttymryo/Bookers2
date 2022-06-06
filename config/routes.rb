619e632b69069763be94ae1284fa2b3462bda27d
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-06 09:32:35 +0000

    [Add]header_link

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
+  root to: 'homes#top'
+  get "home/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :destroy, :show, :edit, :update]
+  resources :users, only: [:show, :edit, :index, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
