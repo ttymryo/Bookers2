469ff28fa575c351ae3d1d24f13c1010cb2bcea0
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-06 09:00:50 +0000

    [Add]model,controller,view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_081907) do
+ActiveRecord::Schema.define(version: 2022_06_06_085510) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
