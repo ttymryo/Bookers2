ab95a548a03d93988b798fc7e9d78bf4d69c897a
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-06 11:44:48 +0000

    [Add]book.destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   private
 
