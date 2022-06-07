c98e2413e9c093b07165d0a7c36d1a57244eb324
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-07 07:14:56 +0000

    [Update]book,user.edit.controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user then
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
